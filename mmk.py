42be52efa994cbe3c4676c2a24777cbb56489712
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-02-21 17:34:21 +0700

    OOP

--- mmk.py
+++ mmk.py
@@ -500,6 +500,380 @@ m(l=16, n=10)
 
 ####################################################################################
 
+1. Procedural programming
+2. OOP
+   - Attributes
+   - tightly couple, "is a"     - hand, engine   <= inheritance
+   - loosely couple, "has a"    - clothes, tire  <= composition
+
+Object-oriented Programming
+
+- a paradigm based on the concept of "objects"
+- paradigm ဂရိစကားလုံး တွေးပုံတွေးနည်း
+- concept အယူအဆ / အမြင်
+- object  အရာဝတ္ထု
+
+အရာဝတ္ထုဆိုတဲ့ အယူအဆအပေါ်မှာ အခြေခံထားတဲ့ တွေးပုံတွေးနည်း ဖြစ်ပါတယ်။
+
+အရာဝတ္ထုလို့တွေးပြီး program ရေးတာ ဖြစ်ပါတယ်။
+
+##################################################
+
+Attributes exercises
+
+အရာဝတ္ထုတွေမှာ ကိုယ်ပိုင် ပိုင်ဆိုင်မှုတွေ ရှိကြပါတယ်။
+
+ဒီနေ့သင်ခန်းစာက ပိုင်ဆိုင်မှုနဲ့ပတ်သတ်ပြီး စနစ်တကျ တွေးတတ်ဖို့ ဖြစ်ပါတယ်။
+
+အဆင့်ဆယ်ဆင့်ရှိပြီး ပထမနှစ်ဆင့်ကို လေ့ကျင့်ပြီး အသားကျမှသာ ကျန်တာတွေ ဆက်လေ့လာလို့ရပါမယ်။
+
+##################################################
+
+1. Write
+
+ပထမအဆင့်ကတော့ အတွေးတွေကို ချရေးတာ ဖြစ်ပါတယ်။
+
+အများကြီးနဲ့ ရှုတ်ရှုတ်ထွေးထွေး မတွေးပဲ နည်းနည်းလေးနဲ့ ရိုးရိုးလေးပဲ တွေးရပါမယ်။
+
+ရိုးရိုးလေးတွေးပြီး ရေးတတ်စေချင်တာပါ။
+
+
+##################################################
+
+2. Divide (data, fun/method)
+
+ဒုတိယအဆင့်ကတော့ ရေးထားတာကို ခွဲထုတ်တာ ဖြစ်ပါတယ်။
+
+ခွဲထုတ်တဲ့အချိန်မှာ အတွေးတွေကို ဖျောက်ထားရမှာဖြစ်ပြီး အသစ်ထပ်ထည့်တာတွေ မလုပ်ရပါဘူး။
+
+ရေးထားတဲ့အတိုင်းဖတ်ပြီး စက်ရုပ်လိုမျိုး ခွဲထုတ်ပေးရပါမယ်။
+
+ခွဲထုတ်ရင်းနဲ့ data ဘယ်နှစ်ခု၊ function ဘယ်နှစ်ခု စသဖြင့် အရေအတွက်အပါအဝင် ပေးထားတဲ့အမည်တွေနဲ့ အလုပ်လုပ်ပုံတွေကိုပါ တစ်ခါတည်း အလွတ်ရအောင် မှတ်ထားဖို့လည်း လိုအပ်ပါသေးတယ်။
+
+မှန်မှန်ကန်ကန် ခွဲထုတ်တတ်ဖို့နဲ့ မှတ်တတ်တဲ့အကျင့် ရှိစေချင်တာပါ။
+
+##################################################
+
+Python ၏ အနှစ်သာရ
+
+1. လှတာ ပိုကောင်းတယ်။
+
+2. ရှင်းလင်းတာ ပိုကောင်းတယ်။
+
+3. အတွေးတွေဟာ ရိုးရှင်းနေရမယ်။
+
+###################################################################################################
+
+Object-oriented Programming
+
+- a paradigm based on the concept of "objects"
+
+#################################################
+
+Attributes exercises
+
+Step.1   --->   Write
+
+7. Car မှာ ကားနံပါတ် တာယာနဲ့ အင်ဂျင်ပါတယ်။ (VIN, tires, engine)
+class   ---> Car
+data    ---> VIN, tires, engine
+method  ---> download(self)
+
+8. Tires ( ကားတာယာ ) တွင် size နှင့် pressure ပါသည်။
+pressure ၏ မူလတန်ဖိုးသည် 0 ( psi ) ဖြစ်သည်။
+လေထိုးသောလုပ်ဆောင်ချက်ပါသည်။ သတ်မှတ်ပေးလိုက်သော ဖိအားအတိုင်း လေထိုးပေးမည်။
+
+9. Engine တွင် fuel_type ပါသည်။
+စက်နှိုး/မနှိုး ဟူသော အခြေအနေ  state ပါသည်။
+မူလအခြေအနေမှာ စက်မနှိုးထားသဖြင့် off ဖြစ်နေမည်။
+ပေးထားသော fuel_type ဖြင့် စက်နှိုး ၊ စက်ရပ် မည့်လုပ်‌ဆောင်ချက်ပါသည်။ ( on(), off() )
+
+###################################################################################################
+
+Step.2   --->   Divide
+
+class   --->
+data    --->
+method  --->
+
+7. Car မှာ ကားနံပါတ် တာယာနဲ့ အင်ဂျင်ပါတယ်။ (VIN, tires, engine)
+class   --->  Car
+data    --->  VIN, tires, engine
+method  --->
+
+8. Tires ( ကားတာယာ ) တွင် size နှင့် pressure ပါသည်။
+pressure ၏ မူလတန်ဖိုးသည် 0 ( psi ) ဖြစ်သည်။
+လေထိုးသောလုပ်ဆောင်ချက်ပါသည်။ သတ်မှတ်ပေးလိုက်သော ဖိအားအတိုင်း လေထိုးပေးမည်။
+class   --->  Tires
+data    --->  size, pressure = 0
+method  --->  pump(p)
+
+def pump():
+    print(f"pump to 0 psi.")
+
+
+def pump(p):
+    print(f"pump to {p} psi.")
+
+
+9. Engine တွင် fuel_type ပါသည်။
+စက်နှိုး/မနှိုး ဟူသော အခြေအနေ  state ပါသည်။
+မူလအခြေအနေမှာ စက်မနှိုးထားသဖြင့် off ဖြစ်နေမည်။
+ပေးထားသော fuel_type ဖြင့် စက်နှိုး ၊ စက်ရပ် မည့်လုပ်‌ဆောင်ချက်ပါသည်။ ( on(), off() )
+class   --->  Engine
+data    --->  fuel_type, state = "off"
+method  --->  on(), off()
+
+###################################################################################################
+
+Car မှာ ကားနံပါတ် တာယာနဲ့ အင်ဂျင်ပါတယ်။ (VIN, tires, engine)
+Tires ( ကားတာယာ ) တွင် size နှင့် pressure ပါသည်။
+pressure ၏ မူလတန်ဖိုးသည် 0 ( psi ) ဖြစ်သည်။
+လေထိုးသောလုပ်ဆောင်ချက်ပါသည်။ သတ်မှတ်ပေးလိုက်သော ဖိအားအတိုင်း လေထိုးပေးမည်။
+Engine တွင် fuel_type ပါသည်။
+စက်နှိုး/မနှိုး ဟူသော အခြေအနေ  state ပါသည်။
+မူလအခြေအနေမှာ စက်မနှိုးထားသဖြင့် off ဖြစ်နေမည်။
+ပေးထားသော fuel_type ဖြင့် စက်နှိုး ၊ စက်ရပ် မည့်လုပ်‌ဆောင်ချက်ပါသည်။ ( on(), off() )
+
+class   --->   Car
+data    --->   VIN, tires, engine
+method  --->
+
+class   --->  Tires
+data    --->  size, pressure = 0
+method  --->  pump(p)
+
+class   --->  Engine
+data    --->  fuel_type, state = off
+method  --->  on(), off()
+
+##################################################################################################
+
+Step.3   --->   Draw design
+
+Prefix data or external data
+
+
+class Tires:
+    def __init__(self):
+        self.pressure = 0         <--- prefix data
+
+
+class Laptop:
+    def __init__(self, x):
+        self.serial_no = x       <--- external data
+
+
+#################################################
+
+Function with obj data
+
+
+def download(self):
+    print("Download with", self.speed)
+
+
+#################################################
+
+Function with obj data + external data 1
+
+
+def pump(self, p):
+    print(f"pump to {p} psi.")
+
+
+#################################################
+
+
+class Tire:
+    def __init__(self, s):
+        self.size = s
+        self.pressure = 0
+
+    def pump(self, p):
+        print(f"Pump to {p} psi.")
+        self.pressure = p
+
+
+t1 = Tire(15)     # Mandalay  4380434 912
+t2 = Tire(18)     # Yangon    4380434 864
+
+print(id(t1))
+print(id(t2))
+
+
+Mandalay
+Mandalay.on the table = book
+
+
+
+4380434912
+4380434864
+
+
+x = Tire(15)     # Mandalay
+y = x            # Mandalay
+
+#################################################
+
+1. House design           <---  paper                   class Tire
+2. Python => Computer     <---  create                  Tire(15)
+3. house1, house2         <---  Mandalay, Yangon        4298171472, 4298171520
+4. Label1                 <---- Mandalay                4298171520
+
+###################################################################################################
+
+
+class Tire:
+    def __init__(self, x):
+        self.pressure = 0
+        self.size = x
+
+    def pump(self, p):
+        print(f"pump to {p} psi.")
+        self.pressure = p
+
+
+class Engine:
+    def __init__(self, fuel_type):
+        self.fuel_type = fuel_type
+        self.state = "off"
+
+    def on(self):
+        print("Engine On.")
+        self.state = "on"
+
+    def off(self):
+        print("Engine Off.")
+        self.state = "off"
+
+
+class Car:
+    def __init__(self, VIN, tires, engine):
+        self.VIN = VIN
+        self.tires = tires
+        self.engine = engine
+
+##################################################################################################
+
+Test city_car
+
+# city_car = Car(VIN=str("BMW-0001"), tires=Tire(15), engine=Engine("petrol"))
+
+city_car = Car("BMW-0001", Tire(15), Engine("petrol"))
+
+print(city_car.tires.pressure)
+print(city_car.engine.state)
+
+city_car.tires.pump(7)
+city_car.engine.on()
+
+print(city_car.tires.pressure)
+print(city_car.engine.state)
+
+##################################################################################################
+
+Step.4   --->  controlling data by function   (state <-- on(), off() )
+
+
+
+class Engine:
+    def __init__(self, fuel_type):
+        self.fuel_type = fuel_type
+        self.state = "off"
+
+    def on(self):
+        print("Engine On.")
+        self.state = "on"
+
+    def off(self):
+        print("Engine Off.")
+        self.state = "off"
+
+        
+##################################################################################################
+
+Step.5   --->  controlling function by data
+
+
+class Engine:
+    def __init__(self, fuel_type):
+        self.fuel_type = fuel_type
+        self.state = "off"
+
+    def on(self):
+        if self.state == "off":
+            print("Engine On.")
+            self.state = "on"
+        else:
+            print("Already On.")
+
+    def off(self):
+        if self.state == "on":
+            print("Engine Off.")
+            self.state = "off"
+        else:
+            print("Already Off.")
+            
+            
+e = Engine("petrol")
+e.on()
+e.on()
+e.on()
+e.on()
+
+##################################################################################################
+
+Step.6   --->  representation string
+
+<__main__.Engine object at 0x10276a6f0>
+<__main__.Engine object at 0x10276a690>
+
+petrol engine
+diesel engine
+
+
+def __repr__(self):
+    return f"<__main__.Engine object at {hex(id(self))}>"
+
+def __repr__(self):
+    return f"{self.fuel_type} engine"
+
+#################################################       
+
+
+class Engine:
+    def __init__(self, fuel_type):
+        self.fuel_type = fuel_type
+        self.state = "off"
+
+    def on(self):
+        if self.state == "off":
+            print("Engine On.")
+            self.state = "on"
+        else:
+            print("Already On.")
+
+    def off(self):
+        if self.state == "on":
+            print("Engine Off.")
+            self.state = "off"
+        else:
+            print("Already Off.")
+
+    def __repr__(self):
+        return f"{self.fuel_type} engine"
+
+
+e = Engine("petrol")
+print(e)
+
+e2 = Engine("diesel")
+print(e2)
+
+##################################################################################################
+
+
 """
 
 
